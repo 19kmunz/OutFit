1ee26f14da6a0e81fdf1c1ffd5cf2e87b2b754a0
K Munz <kijamu@gmail.com>
2021-10-11 22:40:43 -0400

    add search fragment

---
+++ app/src/main/java/edu/wpi/ceflanagan_kjmunz/outfit/SearchFragment.kt
@@ -0,0 +1,22 @@
+package edu.wpi.ceflanagan_kjmunz.outfit
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+private const val TAG = "ClosetFragment"
+
+class SearchFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "onCreateView() called")
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
+        return view;
+    }
+}
